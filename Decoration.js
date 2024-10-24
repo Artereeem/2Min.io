5a1fa7dbc9963f89c560c3da8b2a4b32e0168acf
Artereeem <127101478+Artereeem@users.noreply.github.com>
2024-10-24 22:59:33 +0700

    Add files via upload

--- Decoration.js
+++ Decoration.js
@@ -1,28 +1,22 @@
-
-function onEntry(entry) 
-{
-    entry.ForEach(change=>
-    {
-        if (change.isIntersecting)
-            {
-                change.target.classlist.add('element-show');
-            }
-        else
-        {
-            change.target.classlist.remove('.element-show')
-        }
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+      else
+      {
+          change.target.classList.remove('.element-show')
+      }
     });
-}
-let options = 
-{
-    threshold:[0.5]
-};
-let observer = new
-IntersectionObserver(onEntry,options);
-let elements =
-document.querySelectorAll('.element-animation');
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
 
-for (let elm of elements)
-    {
-        observer.observe(elm);
-    }
+    
